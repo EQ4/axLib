8882d42f9140e112a0a484fc36d7cbe66fb39a67
alarsa <alexandre.arsenault@polymtl.ca>
2015-04-17 12:53:15 -0400

    Fix framework.

--- builds/axLib/Build/Debug/axLib.framework/Versions/A/Headers/axDrawingBuffer.h
+++ builds/axLib/Build/Debug/axLib.framework/Versions/A/Headers/axDrawingBuffer.h
@@ -9,46 +9,46 @@
 #ifndef __axLib__axDrawingBuffer__
 #define __axLib__axDrawingBuffer__
 
-#include "axC++.h"
-#include "cairo/cairo.h"
-#include "axFrameBuffer.h"
-
-class axDrawingBuffer
-{
-public:
-    axDrawingBuffer(const axSize& size);
-    
-    axSize GetSize() const
-    {
-        return _size;
-    }
-    
-    unsigned char* GetData()
-    {
-        return surf_data;
-    }
-    
-    GLuint GetTexture()
-    {
-        return _texture;
-    }
-    
-    void DrawRectangle();
-    
-private:
-     GLuint _frameBuffer, _depthBuffer;
-    GLuint _texture;
-    axSize _size;
-    cairo_surface_t *surface;
-    cairo_t *context;
-    unsigned char* surf_data;
-    
-    cairo_t*  contexteate_cairo_context(int width,
-                                   int height,
-                                   int channels,
-                                   cairo_surface_t** surf,
-                                   unsigned char*& buffer);
-    
-};
+//#include "axC++.h"
+////#include "cairo/cairo.h"
+//#include "axFrameBuffer.h"
+//
+//class axDrawingBuffer
+//{
+//public:
+//    axDrawingBuffer(const axSize& size);
+//    
+//    axSize GetSize() const
+//    {
+//        return _size;
+//    }
+//    
+//    unsigned char* GetData()
+//    {
+//        return surf_data;
+//    }
+//    
+//    GLuint GetTexture()
+//    {
+//        return _texture;
+//    }
+//    
+//    void DrawRectangle();
+//    
+//private:
+//     GLuint _frameBuffer, _depthBuffer;
+//    GLuint _texture;
+//    axSize _size;
+//    cairo_surface_t *surface;
+//    cairo_t *context;
+//    unsigned char* surf_data;
+//    
+//    cairo_t*  contexteate_cairo_context(int width,
+//                                   int height,
+//                                   int channels,
+//                                   cairo_surface_t** surf,
+//                                   unsigned char*& buffer);
+//    
+//};
 
 #endif /* defined(__axLib__axDrawingBuffer__) */
